013feea429991cfd260d91fffcbb15ee848d5bdc
wuxiaokang <sinclairwxk@sina.com>
2018-04-01 15:22:33 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Plan';
 import 'antd/dist/antd.css'
-import registerServiceWorker from './registerServiceWorker';
 document.body.style.height = window.innerHeight + 'px'
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
