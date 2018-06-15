2d27e37ab73cd4b9558278750a44db304b5711bf
wuxiaokang <sinclairwxk@sina.com>
2018-06-15 17:57:18 +0800

    pwa

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Plan';
 import 'antd/dist/antd.css'
+import registerServiceWorker from './registerServiceWorker'
+registerServiceWorker()
 document.body.style.height = window.innerHeight + 'px'
 ReactDOM.render(<App />, document.getElementById('root'));
